2ad96ccbc0ac58e010522bc8a31aa54f93d7f184
Łukasz Blacha <lukasz.blacha@schibsted.pl>
2016-03-10 15:41:24 +0100

    switch to es5 / node 0.12

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-'use strict';
-const argv = require('yargs').argv;
-const fs = require('fs');
-const path = require('path');
-const rmdir = require('rmdir-recursive');
+Object.assign = require('object.assign').getPolyfill();
+var argv = require('yargs').argv;
+var fs = require('fs');
+var path = require('path');
+var rmdir = require('rmdir-recursive');
 
-const Rediff = require('./lib/rediff');
-const config = require(path.join(process.cwd(), argv.config || 'config.js'));
+var Rediff = require('./lib/rediff');
+var config = require(path.join(process.cwd(), argv.config || 'config.js'));
 
 if(Object.keys(config.environments).length !== 2) {
     console.log('ERROR: Rediff expected exactly 2 environments to compare');
     process.exit(1);
 }
 
-let specs;
+var specs;
 if (!argv.spec || argv.spec === 'all') {
     specs = fs.readdirSync(config.specsDir)
-        .map(spec => spec.substr(0, spec.length - 3));
+        .map(function(spec) {
+            return spec.substr(0, spec.length - 3);
+        });
 } else {
     specs = argv.spec.toString().split(' ');
 }
 
 rmdir.sync(config.resultsDir);
 
-let rediff = new Rediff(config, specs);
+var rediff = new Rediff(config, specs);
 rediff.run();
