d5f57989ada263956bf3e7786a7d191410897576
Łukasz Blacha <lukasz.blacha@schibsted.pl>
2016-03-17 17:10:38 +0100

    load css from a separate file

--- examples/config.js
+++ examples/config.js
@@ -27,8 +27,5 @@ module.exports = {
             userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
         }
     },
-    css: {
-        '.random-data-that-breaks-tests': 'display: none !important',
-        'iframe' : 'display: none !important'
-    }
+    cssFile: __dirname + '/exclude.css'
 };
