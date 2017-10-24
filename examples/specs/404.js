72b0afb11d8e83b27bdf97342da5628da40a11fa
Wojciech Iskra <wojciech.iskra@schibsted.com>
2017-10-24 09:54:01 +0200

    Add support for interactive tests

--- examples/specs/404.js
+++ examples/specs/404.js
@@ -2,4 +2,11 @@
 
 module.exports = {
     path: 'non-existent-path',
+    run: async page => {
+        /**
+         * page variable is Puppeteer's page object
+         * https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#class-page
+         */
+        await page.waitForSelector('body');
+    },
 };
